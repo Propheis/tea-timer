989634bc28ad44b2dc26849921a23dd8a18ce024
Kevin Zuern <kevin@kevinzuern.com>
2016-10-02 21:42:58 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  $("#start").click(function() {
+    alert('click');
+  })
+
+  $("")
+
+});
+
+function startClickHandler() {
+
+}
+
+function stopClickHandler() {
+
+}
